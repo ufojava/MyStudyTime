e6abc5811e4ec41e8ec362f6a29a670b88f75c03
Ufuoma Okoro <ufookoro@gmail.com>
2019-09-22 16:01:10 +0100

    Initial Commit

---
+++ MyStudyTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyStudyTime
+//
+//  Created by Ufuoma Okoro on 22/09/2019.
+//  Copyright © 2019 Ufuoma Okoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
