c6c60cf7c457b496b59bb4b997298b759632e418
Ufuoma Okoro <ufookoro@gmail.com>
2019-09-25 22:54:50 +0100

    (1) Created Study Time form - Need to create CoreData and Understand time

--- MyStudyTime/StudyEntry_VC.swift
+++ MyStudyTime/StudyEntry_VC.swift
@@ -12,9 +12,21 @@ import CoreData
 //ViewController for Study Time
 class StudyEntry_VC: UIViewController {
     
-    //Outlet
+    //Outlet for Text field and buttons
     @IBOutlet weak var navigationbarOutletNavbar: UINavigationBar!
     @IBOutlet weak var backToMainMenuItem: UIBarButtonItem!
+    @IBOutlet weak var startTimeOutletText: UITextField!
+    @IBOutlet weak var pickerviewOutletLabel: UILabel!
+    @IBOutlet weak var subjectPickerviewOutletPickerview: UIPickerView!
+    @IBOutlet weak var endTimeOutletText: UITextField!
+    @IBOutlet weak var totalTimeOutletLabel: UILabel!
+    @IBOutlet weak var saveOutletButton: UIButton!
+    @IBOutlet weak var infoSaveOutletLabel: UILabel!
+    
+    //Subject Array
+    var subjectArray = ["English", "Literature","History","Biology","Chemistry","Maths","Physics","Geography"]
+    
+    
     
     
 
@@ -22,7 +34,14 @@ class StudyEntry_VC: UIViewController {
         super.viewDidLoad()
 
         //Custom Function
-        formatVC()
+        formatVC() //Format ViewController
+        conDelData() // Connect Pickerview
+        hideKeyboard() //Hide keyboard
+    }
+    
+    //Function to hide keyboard with gesture
+    func hideKeyboard() {
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
     }
     
     
@@ -52,7 +71,48 @@ class StudyEntry_VC: UIViewController {
         navigationbarOutletNavbar.layer.shadowRadius = 5
         navigationbarOutletNavbar.layer.shadowOffset = CGSize(width: 3, height: 3)
         
+        //Format Text fields, lables and button
+        startTimeOutletText.layer.shadowOpacity = 1
+        startTimeOutletText.layer.shadowRadius = 5
+        startTimeOutletText.layer.shadowOffset = CGSize(width: 3, height: 3)
+        startTimeOutletText.clearButtonMode = .always
+        startTimeOutletText.font = startTimeOutletText.font?.withSize(17)
+        
+        endTimeOutletText.layer.shadowOpacity = 1
+        endTimeOutletText.layer.shadowRadius = 5
+        endTimeOutletText.layer.shadowOffset = CGSize(width: 3, height: 3)
+        endTimeOutletText.clearButtonMode = .always
+        endTimeOutletText.font =
+            endTimeOutletText.font?.withSize(17)
+        
+        
+        totalTimeOutletLabel.textColor = UIColor.blue
+        totalTimeOutletLabel.font = totalTimeOutletLabel.font.withSize(17)
+        
+        saveOutletButton.setTitleColor(.white, for: .normal)
+        saveOutletButton.backgroundColor = UIColor.black
+        saveOutletButton.layer.shadowOpacity = 1
+        saveOutletButton.layer.shadowRadius = 5
+        saveOutletButton.layer.cornerRadius = 10
+        saveOutletButton.layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        
+        totalTimeOutletLabel.layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        totalTimeOutletLabel.layer.shadowOpacity = 1
+        totalTimeOutletLabel.layer.shadowRadius = 5
+        totalTimeOutletLabel.layer.shadowOffset = CGSize(width: 3, height: 3)
+        
+        //Format Boarder for Pickerview
+        subjectPickerviewOutletPickerview.layer.borderColor = UIColor.black.cgColor
+        subjectPickerviewOutletPickerview.layer.borderWidth = 1
+        
                 
+                
+    }
+    
+    //Function to Connect delegate & Datasource
+    func conDelData() {
+        subjectPickerviewOutletPickerview.delegate = self
+        subjectPickerviewOutletPickerview.dataSource = self
     }
     
     //Call Menu
@@ -71,4 +131,28 @@ class StudyEntry_VC: UIViewController {
 
 
 
+}
+
+extension StudyEntry_VC: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    //Number of sections
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    //Number of rows
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return subjectArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return subjectArray[row]
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        pickerviewOutletLabel.textColor = UIColor.purple
+        pickerviewOutletLabel.font = pickerviewOutletLabel.font.withSize(22)
+        pickerviewOutletLabel.text = subjectArray[row]
+    }
 }
