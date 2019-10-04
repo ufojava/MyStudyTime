c6a1bb3922915addeffe8e71dd735e562c1aea42
Ufuoma Okoro <ufookoro@gmail.com>
2019-10-05 00:26:19 +0100

    Completed Targets and save  record update to CoreDate Targets

--- MyStudyTime/StudyTargets_VC.swift
+++ MyStudyTime/StudyTargets_VC.swift
@@ -61,6 +61,14 @@ class StudyTargets_VC: UIViewController {
         
     }
     
+    //Button Action
+    
+    //Save Targets
+    @IBAction func saveTargetsButton(_ sender: UIButton) {
+        recordUpdateNew()
+    }
+    
+    
 
     //Format ViewController
     func formatVC() {
@@ -151,6 +159,8 @@ class StudyTargets_VC: UIViewController {
         saveTargetOutletButton.layer.shadowOffset = CGSize(width: 3, height: 3)
     }
     
+    
+    
      //Function call menu
     @IBAction func backToMenuButtonItem(_ sender: UIBarButtonItem) {
         
@@ -162,6 +172,166 @@ class StudyTargets_VC: UIViewController {
         
         //Call Main Menu
         self.present(mainMenu,animated: true,completion: nil)
+        
+    }
+    
+    //Insert / Update record in CoreData
+    func recordUpdateNew() {
+        
+        //Set Context
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        //Fetch Record
+        let fetcRec = NSFetchRequest<NSFetchRequestResult>(entityName: "Targets")
+        
+        //Set for new record
+        let targetEntity = NSEntityDescription.entity(forEntityName: "Targets", in: context)
+        
+        let newTarget = NSManagedObject(entity: targetEntity!, insertInto: context)
+        
+        //Set fault result
+        fetcRec.returnsObjectsAsFaults = false
+        
+        //Insert record / Update exisiting record
+        
+        do {
+            let results = try context.fetch(fetcRec)
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject] {
+                        
+                        //Retreive English
+                        guard result.value(forKey: "subEnglish") != nil else {
+                            print("English field has nil value")
+                                return
+                        }
+                        //Set Subject English
+                        result.setValue(Int32(subEnglishOutletText.text!), forKey: "subEnglish")
+                        
+                        //Retreive Literature
+                        guard result.value(forKey: "subLiterature") != nil else {
+                            print("Literature field has nil value")
+                            return
+                        }
+                        //Set Subject Literature
+                        result.setValue(Int32(subLiteratureOutletText.text!), forKey: "subLiterature")
+                        
+                        //Retreive History
+                        guard result.value(forKey: "subHistory") != nil else {
+                            print("History has nil value")
+                            return
+                        }
+                        //Set subject History
+                        result.setValue(Int32(subHistoryOutletText.text!), forKey: "subHistory")
+                        
+                        //Retrieve Biology
+                        guard result.value(forKey: "subBiology") != nil else {
+                            print("Unable to reteive Biology")
+                            return
+                        }
+                        //Set subject Biology
+                        result.setValue(Int32(subBiologyOutletText.text!), forKey: "subBiology")
+                        
+                        //Retrieve Chemistry
+                        guard result.value(forKey: "subChemistry") != nil else {
+                            print("Unable to retrieve Chemistry")
+                            return
+                        }
+                        
+                        //Set subject Chemistry
+                        result.setValue(Int32(subChemistryOutletText.text!), forKey: "subChemistry")
+                        
+                        //Retrieve Maths
+                        guard result.value(forKey: "subMaths") != nil else {
+                            print("Unable to retrieve Maths")
+                            return
+                        }
+                        
+                        //Assign subject Maths
+                        result.setValue(Int32(subMathsOutletText.text!), forKey: "subMaths")
+                        
+                        //Retreive Physics
+                        guard result.value(forKey: "subPhysics") != nil else {
+                            print("Unable to retreive Physics")
+                            return
+                        }
+                        //Set subject Physics
+                        result.setValue(Int32(subPhysicsOutletText.text!), forKey: "subPhysics")
+                        
+                        //Retrieve Geography
+                        guard result.value(forKey: "subGeography") != nil else {
+                            print("Unable to retrieve Geography")
+                            return
+                        }
+                        //Set subject Geography
+                        result.setValue(Int32(subGeographyOutletText.text!), forKey: "subGeography")
+                        
+                        
+                        //Send save message
+                        infoTargetsOutletLabel.textColor = UIColor.blue
+                        infoTargetsOutletLabel.textAlignment = .center
+                        infoTargetsOutletLabel.text = "Subject Tagets Set"
+                                         
+                        //Delay the infor screen
+                                         
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+                        self.infoTargetsOutletLabel.text = ""
+                            
+                        //Reset Screen
+                        self.subEnglishOutletText.text = ""
+                        self.subLiteratureOutletText.text = ""
+                        self.subHistoryOutletText.text = ""
+                        self.subBiologyOutletText.text = ""
+                        self.subChemistryOutletText.text = ""
+                        self.subMathsOutletText.text = ""
+                        self.subPhysicsOutletText.text = ""
+                        self.subGeographyOutletText.text = ""
+                        }
+                        
+                    }
+                } else { //Enter new record from here if no record exist
+                    newTarget.setValue(subEnglishOutletText.text, forKey: "subEnglish")
+                    newTarget.setValue(subLiteratureOutletText.text, forKey: "subLiterature")
+                    newTarget.setValue(subHistoryOutletText.text, forKey: "subHistory")
+                    newTarget.setValue(subBiologyOutletText.text, forKey: "subBiology")
+                    newTarget.setValue(subChemistryOutletText.text, forKey: "subChemistry")
+                    newTarget.setValue(subMathsOutletText.text, forKey: "subMaths")
+                    newTarget.setValue(subPhysicsOutletText.text, forKey: "subPhysics")
+                    newTarget.setValue(subGeographyOutletText.text, forKey: "subGeography")
+                    
+                    //Send save message
+                    infoTargetsOutletLabel.textColor = UIColor.yellow
+                    infoTargetsOutletLabel.textAlignment = .center
+                    infoTargetsOutletLabel.text = "Subject Tagets Set"
+                    
+                    //Delay the information screen
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+                        self.infoTargetsOutletLabel.text = ""
+                        
+                        //Reset Screen
+                        self.subEnglishOutletText.text = ""
+                        self.subLiteratureOutletText.text = ""
+                        self.subHistoryOutletText.text = ""
+                        self.subBiologyOutletText.text = ""
+                        self.subChemistryOutletText.text = ""
+                        self.subMathsOutletText.text = ""
+                        self.subPhysicsOutletText.text = ""
+                        self.subGeographyOutletText.text = ""
+                    }
+                    
+            }
+            
+        } catch {
+            print("Unable to fetch record")
+        }
+        
+        //Save record in CoreDate
+        do {
+            try context.save()
+        } catch {
+            
+            print("Unable to save record")
+        }
         
     }
     
